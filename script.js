714c9ef646044020f6076ce5059f3434ac5c6b50
Sochacki <M.Sochacki@funkegruppe.de>
2024-07-09 15:55:25 +0200

    Fixing minor bugs and changes

--- script.js
+++ script.js
@@ -2,16 +2,28 @@ google.charts.load("current", {packages:["corechart"]});
 google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
   var data = google.visualization.arrayToDataTable([
-    ['Task', 'Hours per Day'],
-    ['Work',     11],
-    ['Eat',      2],
-    ['Commute',  2],
-    ['Watch TV', 2],
-    ['Sleep',    7]
+    ['Kategoria wydatku', 'Suma z wybranego okresu'],
+    ['Jedzenie', 400.00],
+    ['Mieszkanie', 1450.00],
+    ['Transport', 300.00],
+    ['Telekomunikacja', 100.00],
+    ['Opieka zdrowotna', 150.00],
+    ['Ubranie', 150.00],
+    ['Higienia', 50.00],
+    ['Dzieci', 0],
+    ['Rozrywka', 150.00],
+    ['Wycieczka', 70.00],
+    ['Szkolenia', 1000.00],
+    ['Książki', 25.00],
+    ['Oszczędności', 100.00],
+    ['Na złotą jesień, czyli emeryturę', 100.00],
+    ['Spłata długów', 0.00],
+    ['Darowizna', 0.00],
+    ['Inne wydatki', 150.00]
   ]);
 
   var options = {
-    title: 'My Daily Activities',
+    title: 'Wydatki',
     is3D: true,
   };
 
