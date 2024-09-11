e1cd2e1aa95704be65086b03811e910f154de307
Sochacki <M.Sochacki@funkegruppe.de>
2024-09-11 14:56:29 +0200

    Workin on estetical site of the website in add-expanse.php

--- add-expanse.php
+++ add-expanse.php
@@ -1,27 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Dodaj wydatek</title>
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-      crossorigin="anonymous"
-    />
-    <link rel="stylesheet" href="./style.css" />
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-      crossorigin="anonymous"
-    ></script>
-  </head>
-  <body>
-    <nav
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Dodaj wydatek</title>
+  <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+    crossorigin="anonymous" />
+  <link rel="stylesheet" href="./style.css" />
+  <script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+</head>
+
+<body>
+  <nav
     class="navbar navbar-expand-sm navbar-dark bg-dark"
-    aria-label="Third navbar example"
-  >
+    aria-label="Third navbar example">
     <div class="container-fluid">
       <button
         class="navbar-toggler collapsed"
@@ -30,8 +29,7 @@
         data-bs-target="#navbarsExample03"
         aria-controls="navbarsExample03"
         aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
+        aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
@@ -43,9 +41,7 @@
           <li class="nav-item">
             <a
               class="nav-link"
-              href="./sign-up-page.php"
-              >Rejestracja</a
-            >
+              href="./sign-up-page.php">Rejestracja</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="./log-in-page.php">Logowanie</a>
@@ -63,155 +59,81 @@
       </div>
     </div>
   </nav>
-    <div class="container">
+  <div class="container">
+    <form action="">
       <div class="row justify-content-center">
         <div class="col-sm-6">
           <div class="mb-3 mt-3">
-            <label for="exampleFormControlInput1" class="form-label"
-              >Kwota</label
-            >
+            <label for="exampleFormControlInput1" class="form-label">Kwota</label>
             <input
               type="number"
               class="form-control"
               id="exampleFormControlInput1"
               placeholder="12.14"
-            />
+              name="amount" />
           </div>
           <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label"
-              >Data</label
-            >
+            <label for="exampleFormControlInput1" class="form-label">Data</label>
             <input
               type="date"
               class="form-control"
               id="exampleFormControlInput2"
               placeholder="2024.12.12"
-            />
+              name="date" />
           </div>
           <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label"
-              >Komentarz</label
-            >
+            <label for="exampleFormControlInput1" class="form-label">Komentarz</label>
             <input
               type="text"
               class="form-control"
               id="exampleFormControlInput2"
               placeholder="(Opcjonalnie)"
-            />
+              name="comment" />
           </div>
         </div>
-        <div class="px-4 pt-3 pb-3 text-center">
-          <div class="dropdown">
-            <button
-              class="btn btn-primary btn-lg px-4 me-sm-3 mb-3 dropdown-toggle"
-              type="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Wybierz sposób płatności
-            </button>
-            <ul class="dropdown-menu">
-              <li>
-                <button class="dropdown-item" type="button">Gotówka</button>
-              </li>
-              <li>
-                <button class="dropdown-item" type="button">
-                  Karta debetowa
-                </button>
-              </li>
-              <li>
-                <button class="dropdown-item" type="button">
-                  Karta kredytowa
-                </button>
-              </li>
-            </ul>
-          </div>
-          <div class="dropdown">
-            <button
-              class="btn btn-primary btn-lg px-4 me-sm-3 dropdown-toggle"
-              type="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              Wybierz rodzaj wydatku
-            </button>
-            <ul class="dropdown-menu">
-              <li>
-                <button class="dropdown-item" type="button">Jedzenie</button>
-              </li>
-              <li>
-                <button class="dropdown-item" type="button">Mieszkanie</button>
-              </li>
-              <li>
-                <button class="dropdown-item" type="button">Transport</button>
-              </li>
-              <li>
-                <button class="dropdown-item" type="button">
-                  Telekomunikacja
-                </button>
-              </li>
-              <li>
-                <button class="dropdown-item" type="button">
-                  Opieka zdrowotna
-                </button>
-              </li>
-              <li>
-                <button class="dropdown-item" type="button">Ubranie</button>
-              </li>
-              <li>
-                <button class="dropdown-item" type="button">Higienia</button>
-              </li>
-              <li>
-                <button class="dropdown-item" type="button">Dzieci</button>
-              </li>
-              <li>
-                <button class="dropdown-item" type="button">Rozrywka</button>
-              </li>
-              <li>
-                <button class="dropdown-item" type="button">Wycieczka</button>
-              </li>
-              <li>
-                <button class="dropdown-item" type="button">Szkolenia</button>
-              </li>
-              <li>
-                <button class="dropdown-item" type="button">Książki</button>
-              </li>
-              <li>
-                <button class="dropdown-item" type="button">
-                  Oszczędności
-                </button>
-              </li>
-              <li>
-                <button class="dropdown-item" type="button">
-                  Na złotą jesień, czyli emeryturę
-                </button>
-              </li>
-              <li>
-                <button class="dropdown-item" type="button">
-                  Spłata długów
-                </button>
-              </li>
-              <li>
-                <button class="dropdown-item" type="button">Darowizna</button>
-              </li>
-              <li>
-                <button class="dropdown-item" type="button">
-                  Inne wydatki
-                </button>
-              </li>
-            </ul>
+        <div class="row text-center">
+          <div class="col">
+            <div>
+              <select class="btn btn-primary btn-lg px-4 me-sm-3 mb-3" name="paymentMethod" id="paymentMethod">
+                <option value="" disabled selected>Wybierz sposób płatności</option>
+                <option value="Gotówka">Gotówka</option>
+                <option value="Karta debetowa">Karta debetowa</option>
+                <option value="Karta kredytowa">Karta kredytowa</option>
+              </select>
+            </div>
+            <div>
+              <select class="btn btn-primary btn-lg px-4 me-sm-3 mb-3" name="expandCategory" id="paymentMethod">
+                <option value="" disabled selected>Wybierz kategorię wydatku</option>
+                <option value="Jedzenie">Jedzenie</option>
+                <option value="Mieszkanie">Mieszkanie</option>
+                <option value="Transport">Transport</option>
+                <option value="Telekomunikacja">Telekomunikacja</option>
+                <option value="Opieka zdrowotna">Opieka zdrowotna</option>
+                <option value="Ubranie">Ubranie</option>
+                <option value="Higiena">Higiena</option>
+                <option value="Dzieci">Dzieci</option>
+                <option value="Rozrywka">Rozrywka</option>
+                <option value="Wycieczka">Wycieczka</option>
+                <option value="Szkolenia">Szkolenia</option>
+                <option value="Książki">Książki</option>
+                <option value="Na złotą jesień, czyli emeryturę">Na złotą jesień, czyli emeryturę</option>
+                <option value="Spłata długów">Spłata długów</option>
+                <option value="Darowizna">Darowizna</option>
+                <option value="Inne wydatki">Inne wydatki</option>
+              </select>
+            </div>
           </div>
         </div>
         <div class="row text-center">
           <div class="col-sm">
             <button class="yes-button btn btn-primary btn-lg px-4 me-sm-3 mb-3">
-              Dodaj wydatek</button
-            ><button class="no-button btn btn-primary btn-lg px-4 me-sm-3 mb-3">
+              Dodaj wydatek</button><button class="no-button btn btn-primary btn-lg px-4 me-sm-3 mb-3">
               Anuluj
             </button>
           </div>
         </div>
-      </div>
-    </div>
-  </body>
+    </form>
+  </div>
+</body>
+
 </html>
