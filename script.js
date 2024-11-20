c4777a16e12d7b621b8191a9e45f7fdc98ca03a8
Sochacki <M.Sochacki@funkegruppe.de>
2024-11-20 15:47:04 +0100

    Changin script.js, deleting chart

--- script.js
+++ script.js
@@ -1,40 +1,3 @@
-google.charts.load("current", {packages:["corechart"]});
-google.charts.setOnLoadCallback(drawChart);
-function drawChart() {
-  var data = google.visualization.arrayToDataTable([
-    ['Kategoria wydatku', 'Suma z wybranego okresu'],
-    ['Jedzenie', 400.00],
-    ['Mieszkanie', 1450.00],
-    ['Transport', 300.00],
-    ['Telekomunikacja', 100.00],
-    ['Opieka zdrowotna', 150.00],
-    ['Ubranie', 150.00],
-    ['Higienia', 50.00],
-    ['Dzieci', 0],
-    ['Rozrywka', 150.00],
-    ['Wycieczka', 70.00],
-    ['Szkolenia', 1000.00],
-    ['Książki', 25.00],
-    ['Oszczędności', 100.00],
-    ['Na złotą jesień, czyli emeryturę', 100.00],
-    ['Spłata długów', 0.00],
-    ['Darowizna', 0.00],
-    ['Inne wydatki', 150.00]
-  ]);
-
-  var options = {
-    title: 'Wydatki',
-    is3D: true,
-  };
-
-  var chart = new google.visualization.PieChart(document.getElementById('piechart_3d'));
-  chart.draw(data, options);
-}
-
-$(window).resize(function(){
-  drawChart();
-});
-
 function showAnotherDiv() {
   var divToDisplay = document.getElementById("divToDisplay");
   if (document.getElementById("timePeriod").value == "custom")
@@ -44,26 +7,5 @@ function showAnotherDiv() {
   else
   {
     divToDisplay.hidden  = true;
-  }
-}
-
-function selectPreviousValue(frTimePeriod) {
-  document.getElementById("timePeriod").value = frTimePeriod;
-}
-
-document.getElementById("showBalanceForm").onsubmit = function() {showHiddenTables()};
-
-function showHiddenTables() {
-  var tablesToDisplay = document.getElementsByClassName("table");
-  for(i = 0; i < tablesToDisplay.length; i++)
-  {
-    if(tablesToDisplay[i].hidden == true)
-    {
-      tablesToDisplay[i].hidden = false;
-    }
-    else
-    {
-      tablesToDisplay[i].hidden = true;
-    }
   }
 }
